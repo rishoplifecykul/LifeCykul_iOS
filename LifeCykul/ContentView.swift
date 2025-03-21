2223980b4eca9eae3830b09848b8bea7a428a4c5
Rishop Babu <rishopbabu22@gmail.com>
2025-03-21 15:13:21 +0530

    Initial Commit

---
+++ LifeCykul/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeCykul
+//
+//  Created by Rishop Babu on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LifeCykul/LifeCykulApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCykulApp.swift
+//  LifeCykul
+//
+//  Created by Rishop Babu on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeCykulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
